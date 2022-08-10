9c95887b0766d001190e38553e7b9ea44915926f
john-waczak <john.louis.waczak@gmail.com>
2022-08-09 21:58:55 -0500

    fixing name typo

---
+++ scripts/RobotTeam2022/make_datasets.jl
@@ -0,0 +1,46 @@
+using mintsML
+using MLJ
+using DataFrames, CSV
+using ProgressMeter
+
+
+include("./config.jl")
+include("./utils.jl")
+
+outpath = "/media/john/HSDATA/datasets"
+ispath(outpath)
+
+function make_datasets(datapath, outpath)
+    @showprogress for (target, info) ∈ targetsDict
+        # make output
+        target_name = String(target)
+        date = split(datapath, "/")[end]
+        outpath_base = joinpath(outpath, date, target_name)
+
+        # make sure outpath exists
+        if !isdir(outpath_base)
+            println("making folder $(outpath_base)")
+            mkpath(outpath_base)
+        end
+
+        #(y, X), (yval, Xval), (ytest, Xtest) = makeDatasets(datapath, target)
+        (y, X), (ytest, Xtest) = makeDatasets(datapath, target)
+
+        # now we want to save the data.
+        CSV.write(joinpath(outpath_base, "X.csv"), X)
+        CSV.write(joinpath(outpath_base, "y.csv"), DataFrame(Dict(target => y)))
+
+        # CSV.write(joinpath(outpath_base, "Xval.csv"), Xval)
+        # CSV.write(joinpath(outpath_base, "yval.csv"), DataFrame(Dict(target => yval)))
+
+        CSV.write(joinpath(outpath_base, "Xtest.csv"), Xtest)
+        CSV.write(joinpath(outpath_base, "ytest.csv"), DataFrame(Dict(target => ytest)))
+
+    end
+end
+
+# do it for 11-23
+datapath = "/media/john/HSDATA/processed/11-23"
+ispath(datapath)
+
+make_datasets(datapath, outpath)
