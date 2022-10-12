a34756c32a3227f46b03a05b6fe8b4b6795a04f2
john-waczak <john.louis.waczak@gmail.com>
2022-10-12 09:19:22 -0500

    updated slurm generation script for EvoTrees

--- scripts/RobotTeam2022/make_slurm_scripts.jl
+++ scripts/RobotTeam2022/make_slurm_scripts.jl
@@ -63,15 +63,22 @@ end
 #                 )
 
 
+# make_slurm_jobs(;
+#                 script_to_run="5c__hpo_XGBoostRegressor.jl",
+#                 basename="5c_hpo_",
+#                 n_tasks=4,
+# 		            datapath="/scratch/jwaczak/data/datasets/Full",
+# 		            outpath="/scratch/jwaczak/data/analysis_full",
+#                 )
+
 
 make_slurm_jobs(;
-                script_to_run="5c__hpo_XGBoostRegressor.jl",
-                basename="5c_hpo_",
+                script_to_run="5e__hpo_EvoTreeRegressor.jl",
+                basename="5e_hpo_",
                 n_tasks=4,
 		            datapath="/scratch/jwaczak/data/datasets/Full",
 		            outpath="/scratch/jwaczak/data/analysis_full",
                 )
-
 
 
 
